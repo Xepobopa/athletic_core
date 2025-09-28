37b71c8c0dbc34e11f39cc083cfe07a40720fecf
Xepobopa <ismartsdn4477@gmail.com>
2025-09-28 20:00:19 +0200

    fix paddings in the mobile version

--- src/components/Training.tsx
+++ src/components/Training.tsx
@@ -11,10 +11,10 @@ export default function Training({ text, image, schedule }: TrainingProps) {
  return (
         <div className="global-px">
             <section className="flex flex-wrap text-center pt-[20px] gap-x-[100px]">
-              <div className="flex-1 min-w-[400px] flex flex-col justify-center items-center gap-y-[15px]">
+              <div className="flex-1 min-w-[300px] flex flex-col justify-center items-center gap-y-[15px]">
                 <p className="font-[400] text-[24px] lg:text-[32px]">{text}</p>
               </div>
-              <div className="flex-1 min-w-[400px] flex flex-col items-center">
+              <div className="flex-1 min-w-[300px] flex flex-col items-center">
                 <Image alt="list" src={image} />
                 {/* <Image className="absolute w-[55%] bottom-0 right-0 z-30 pt-[100%]" alt="list" src={Images.Children2} /> */}
               </div>
